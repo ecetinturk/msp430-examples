c8a529f0cf50a87cf3093dbb2225b78fd77d2a27
Emirhan <101136726+ecetinturk@users.noreply.github.com>
2022-08-18 15:40:03 +0300

    Create adc12.h

---
+++ 10_Accelerometer/driverApi/adc12.h
@@ -0,0 +1,18 @@
+#ifndef DRIVERAPI_ADC12_H_
+#define DRIVERAPI_ADC12_H_
+
+/**************************
+ * Libraries
+ **************************/
+//#include "driverlib.h"
+#include "adc12_a.h"
+#include "ref.h"
+#include "gpio.h"
+
+/**************************
+ * Function declarations
+ **************************/
+void ConfigADC12(void);
+
+
+#endif /* DRIVERAPI_ADC12_H_ */
